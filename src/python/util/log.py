7d14d9114aef8ae0c2ea9b549e7569fb76fb6f9e
Leo Meyerovich <leo@graphistry.com>
2020-09-01 01:26:30 -0700

    fix(dockerfile): namespacing and no phone home

--- src/python/util/log.py
+++ src/python/util/log.py
@@ -4,7 +4,7 @@ logger = logging.getLogger()
 logger.setLevel(
     logging.getLevelName(os.environ['LOG_LEVEL'])
     if 'LOG_LEVEL' in os.environ else 
-    logging.error)
+    logging.ERROR)
 
 def getChild(*args, **kwargs):
     return logger.getChild(*args, **kwargs)
