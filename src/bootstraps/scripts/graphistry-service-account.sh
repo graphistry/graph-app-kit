95cf11bc21366277212ab735acb90cbba9933884
Leo Meyerovich <leo@graphistry.com>
2020-11-12 15:42:14 -0800

    fix(service account): unique email

--- src/bootstraps/scripts/graphistry-service-account.sh
+++ src/bootstraps/scripts/graphistry-service-account.sh
@@ -25,8 +25,8 @@ POST_SCRIPT="CELERY_BROKER_URL=zz python manage.py shell && echo done || { echo 
 ## Service account
 export SERVICE_USER=graphappkit
 export SERVICE_PASS="${INSTANCE_ID}_${RANDOM}"
-ADD_USER_SCRIPT="from nexus.users.models import User; user=User.objects.create_superuser(username='${SERVICE_USER}', email='admin@amazon.com', password='${SERVICE_PASS}', name='${SERVICE_USER}', is_active=True); print('made service account ${SERVICE_USER}')"
-VERIFY_USER_SCRIPT="from allauth.account.models import EmailAddress; e = EmailAddress.objects.create(user=user, email='admin@amazon.com', primary=True, verified=True); e.save(); print('verified user')"
+ADD_USER_SCRIPT="from nexus.users.models import User; user=User.objects.create_superuser(username='${SERVICE_USER}', email='root@amazon.com', password='${SERVICE_PASS}', name='${SERVICE_USER}', is_active=True); print('made service account ${SERVICE_USER}')"
+VERIFY_USER_SCRIPT="from allauth.account.models import EmailAddress; e = EmailAddress.objects.create(user=user, email='root@amazon.com', primary=True, verified=True); e.save(); print('verified user')"
 POST_SCRIPT="CELERY_BROKER_URL=zz python manage.py shell && echo done || { echo fail && exit 1; }"
 
 ( \
