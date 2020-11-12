46cfcbfb62d674acc3620f6a4f9cd74e4fd164ef
Leo Meyerovich <leo@graphistry.com>
2020-11-12 15:12:39 -0800

    fix(neptune): typo

--- src/bootstraps/scripts/graphistry-service-account.sh
+++ src/bootstraps/scripts/graphistry-service-account.sh
@@ -7,7 +7,7 @@ SCRIPT="Get AWS Instance ID"
 
 ADD_USER_SCRIPT="from nexus.users.models import User; user=User.objects.create_superuser(username='admin', email='admin@amazon.com', password='${INSTANCE_ID}', name='admin', is_active=True); print('made admin')"
 VERIFY_USER_SCRIPT="from allauth.account.models import EmailAddress; e = EmailAddress.objects.create(user=user, email='admin@amazon.com', primary=True, verified=True); e.save(); print('verified admin')"
-POST_SCRIPT = "CELERY_BROKER_URL=zz python manage.py shell && echo done || { echo fail && exit 1; }"
+POST_SCRIPT="CELERY_BROKER_URL=zz python manage.py shell && echo done || { echo fail && exit 1; }"
 
 ## Admin account (user should change on login)
 #FIXME Faster and safer by by a nexus ready check (migrations done => server started?)
