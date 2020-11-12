4e52c310c5f8804bb4b415e3773e5361c67abfab
Leo Meyerovich <leo@graphistry.com>
2020-11-12 15:25:38 -0800

    fix(neptune automation): actually create service account

--- src/bootstraps/scripts/graphistry-service-account.sh
+++ src/bootstraps/scripts/graphistry-service-account.sh
@@ -27,6 +27,13 @@ export SERVICE_USER=graphappkit
 export SERVICE_PASS="${INSTANCE_ID}_${RANDOM}"
 ADD_USER_SCRIPT="from nexus.users.models import User; user=User.objects.create_superuser(username='${SERVICE_USER}', email='admin@amazon.com', password='${SERVICE_PASS}', name='${SERVICE_USER}', is_active=True); print('made service account ${SERVICE_USER}')"
 VERIFY_USER_SCRIPT="from allauth.account.models import EmailAddress; e = EmailAddress.objects.create(user=user, email='admin@amazon.com', primary=True, verified=True); e.save(); print('verified user')"
-POST_SCRIPT = "CELERY_BROKER_URL=zz python manage.py shell && echo done || { echo fail && exit 1; }"
+POST_SCRIPT="CELERY_BROKER_URL=zz python manage.py shell && echo done || { echo fail && exit 1; }"
+
+( \
+    cd "${GRAPHISTRY_HOME}" \
+    && docker-compose exec -T nexus \
+        bash -c \
+          "source activate rapids && echo \"${ADD_USER_SCRIPT}; ${VERIFY_USER_SCRIPT}\" | ${POST_SCRIPT}" \
+)
 
 ./hello-end.sh "$SCRIPT"
