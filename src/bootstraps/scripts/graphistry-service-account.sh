8503d283f9ccff014ff4fa184187e7064cb1be7a
Leo Meyerovich <leo@graphistry.com>
2021-01-12 18:36:06 -0800

    feat(reuse existing admin account): graphistry 2.34 creates aws admin

--- src/bootstraps/scripts/graphistry-service-account.sh
+++ src/bootstraps/scripts/graphistry-service-account.sh
@@ -5,21 +5,14 @@ SCRIPT="Get AWS Instance ID"
 ./hello-start.sh "$SCRIPT"
 
 
-ADD_USER_SCRIPT="from nexus.users.models import User; user=User.objects.create_superuser(username='admin', email='admin@amazon.com', password='${INSTANCE_ID}', name='admin', is_active=True); print('made admin')"
-VERIFY_USER_SCRIPT="from allauth.account.models import EmailAddress; e = EmailAddress.objects.create(user=user, email='admin@amazon.com', primary=True, verified=True); e.save(); print('verified admin')"
-POST_SCRIPT="CELERY_BROKER_URL=zz python manage.py shell && echo done || { echo fail && exit 1; }"
-
-## Admin account (user should change on login)
-#FIXME Faster and safer by by a nexus ready check (migrations done => server started?)
+### Graphistry should already add admin / <i-instanceid> on boot
 ( \
     cd "${GRAPHISTRY_HOME}" \
     && ( \
         until ( curl -fsS http://localhost/streamgl-gpu/secondary/gpu/health > /dev/null ); \
         do ( docker-compose ps && sleep 1 ); \
         done \
-    ) && docker-compose exec -T nexus \
-        bash -c \
-          "source activate rapids && echo \"${ADD_USER_SCRIPT}; ${VERIFY_USER_SCRIPT}\" | ${POST_SCRIPT}" \
+    )
 )
 
 ## Service account
