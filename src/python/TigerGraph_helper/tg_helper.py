b9e61d4bcd8ffca37c55817101ba3c660d09c3dd
Leo Meyerovich <leo@graphistry.com>
2021-02-24 14:02:51 -0800

    garden(tg): Clearer connection progress logging

--- src/python/TigerGraph_helper/tg_helper.py
+++ src/python/TigerGraph_helper/tg_helper.py
@@ -1,27 +1,35 @@
-# Function to connect to TigerGraph
-
-import os
-import logging
-import pyTigerGraphBeta as tg
-
-logger = logging.getLogger()
+import logging, os, pyTigerGraphBeta as tg
+from typing import Optional
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+def connect_to_tigergraph() -> Optional[dict]:
+    if ('TIGERGRAPH_HOST' in os.environ and 'TIGERGRAPH_USERNAME' in os.environ \
+        and 'TIGERGRAPH_PASSWORD' in os.environ and 'TIGERGRAPH_GRAPHNAME' in os.environ):
 
-def connect_to_tigergraph():
-    if ('TIGERGRAPH_HOST' in os.environ and 'TIGERGRAPH_USERNAME' in os.environ and 'TIGERGRAPH_PASSWORD' in os.environ and 'TIGERGRAPH_GRAPHNAME' in os.environ):
-        host = os.environ["TIGERGRAPH_HOST"]
-        username = os.environ["TIGERGRAPH_USERNAME"]
-        password = os.environ["TIGERGRAPH_PASSWORD"]
-        graphname = os.environ["TIGERGRAPH_GRAPHNAME"]
-        conn = tg.TigerGraphConnection(host=host, graphname=graphname, username=username, password=password)
+        creds = {
+            'host': os.environ["TIGERGRAPH_HOST"],
+            'username': os.environ["TIGERGRAPH_USERNAME"],
+            'password': os.environ["TIGERGRAPH_PASSWORD"],
+            'graphname': os.environ["TIGERGRAPH_GRAPHNAME"]
+        }
+        logger.info('Connecting to TigerGraph using environment variables: %s',
+            {**creds,
+            'password': ''.join(['*' for x in creds['password']]) if creds['password'] is not None else ''})
+
+        conn = tg.TigerGraphConnection(**creds)
+
         if ('TIGERGRAPH_SECRET' in os.environ and os.environ["TIGERGRAPH_SECRET"] is not None):
+            logger.info('... Connected to TG, getting token via provided secret...')
             secret = os.environ["TIGERGRAPH_SECRET"]
             conn.getToken(secret)
         else:
+            #FIXME: This times out in practice, maybe TG 3.0 -> 3.1 version issues?
+            logger.info('... Connected to TG, creating secret and getting token...')
             conn.getToken(conn.createSecret())
-        logger.info(host)
+
+        logger.info('Successfully finished connecting to TG!')
         return conn
-    else:
-        logging.error("Internal Configuraiton Error Occurred.  ")
-        return None
+
+    logger.debug("Missing TigerGraph environment variables; skipping connection")
+    return None
