132b17eb659dd1b618b84456f080cef1cac26169
Thomas Cook <tcook@graphistry.com>
2023-08-13 19:17:45 -0500

    add new code for better logger handling for streamlit

--- src/python/util/log.py
+++ src/python/util/log.py
@@ -1,9 +1,36 @@
 import logging
 import os
+import streamlit.logger
+
+streamlit.logger.get_logger = logging.getLogger
+streamlit.logger.setup_formatter = None
+streamlit.logger.update_formatter = lambda *a, **k: None
+streamlit.logger.set_log_level = lambda *a, **k: None
+
+# https://github.com/streamlit/streamlit/issues/4742#issuecomment-1130917745
+# Then set our logger in a normal way
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s %(asctime)s %(name)s:%(message)s",
+    #force=True,
+)  # Change these settings for your own purpose, but keep force=True at least
+
+streamlit_handler = logging.getLogger("streamlit")
+streamlit_handler.setLevel(logging.DEBUG)
 
 logger = logging.getLogger()
-logger.setLevel(logging.getLevelName(os.environ["LOG_LEVEL"]) if "LOG_LEVEL" in os.environ else logging.ERROR)
 
+# disable DEBUG for watchdog - too verbose
+logging.getLogger("watchdog").setLevel(logging.WARNING)
+
+if "LOG_LEVEL" in os.environ:
+    log_level=getattr(logging, os.environ["LOG_LEVEL"].upper())
+else:
+    log_level=getattr(logging,logging.ERROR)
+
+logger.setLevel(log_level)
 
 def getChild(*args, **kwargs):
-    return logger.getChild(*args, **kwargs)
+    out=logger.getChild(*args, **kwargs)
+    out.setLevel(log_level)
+    return out
