37e62c2ec6fc3f16b4cf9af17ba8b8e9fe5d74fc
Leo Meyerovich <leo@graphistry.com>
2023-10-18 12:45:04 -0400

    fix(log level): handling when no env var

--- src/python/util/log.py
+++ src/python/util/log.py
@@ -8,13 +8,9 @@ def getChild(*args, **kwargs):
 
     logger = logging.getLogger('gak')
 
-    if "LOG_LEVEL" in os.environ:
-        log_level=getattr(logging, os.environ["LOG_LEVEL"].upper())
-    else:
-        log_level=getattr(logging,logging.ERROR)
-
-        logger.debug(f"util.log envar LOG_LEVEL: {os.environ['LOG_LEVEL']}")
-        logger.debug(f"util.log log_level == {log_level}")
+    log_level_str = os.environ.get('LOG_LEVEL', 'ERROR').upper()
+    log_level = getattr(logging, log_level_str)
+    logger.debug(f"util.log log_level == {log_level_str} ({log_level})")
 
     out=logger.getChild(*args, **kwargs)
     out.setLevel(log_level)
